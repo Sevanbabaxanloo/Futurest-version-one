4bb35ebf01f006fc39b5b90bafcd7276a4f71c9f
Sevan Babaxanloo <sevan_babaxanloo@yahoo.com>
2025-01-02 23:33:25 +0400

    final

--- pages/top-event/top-event.js
+++ pages/top-event/top-event.js
@@ -11,23 +11,6 @@ function checkScroll() {
 
 window.addEventListener("scroll", checkScroll);
 
-// start of slider
-
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  speed: 1500,
-});
-
 // question pop up
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -68,4 +51,95 @@ document.addEventListener("DOMContentLoaded", function () {
       hidePopup();
     }
   });
+});
+
+// Mobile menu function and animation
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+  const body = document.body;
+
+  menu.classList.add("mobile-menu-hidden");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("open");
+    if (menu.classList.contains("mobile-menu-hidden")) {
+      menu.classList.remove("mobile-menu-hidden");
+      menu.classList.add("mobile-menu-visible");
+      body.classList.add("mobile-menu-scroll");
+    } else {
+      menu.classList.remove("mobile-menu-visible");
+      menu.classList.add("mobile-menu-hidden");
+      body.classList.remove("mobile-menu-scroll");
+    }
+  });
+});
+
+//start of search animation function
+
+const searchInput = document.getElementById("searchInput");
+const redText = document.getElementById("redText");
+const greenText = document.getElementById("greenText");
+
+if (searchInput) {
+  searchInput.addEventListener("focus", function () {
+    redText.style.transform = "translateY(50%)";
+    greenText.style.opacity = 0;
+    greenText.style.transform = "translateY(50%)";
+  });
+
+  searchInput.addEventListener("input", function () {
+    redText.style.opacity = this.value ? 0 : 1;
+  });
+
+  searchInput.addEventListener("blur", function () {
+    if (!this.value) {
+      redText.style.transform = "translateY(0)";
+      greenText.style.opacity = 1;
+      greenText.style.transform = "translateY(0)";
+    }
+  });
+}
+
+// Open QR pop up function
+
+document.querySelectorAll(".toggleButton").forEach(function (element) {
+  element.addEventListener("click", function () {
+    const toggleDivContainer =
+      document.querySelector("#toggleDiv").parentElement;
+    const body = document.body;
+    if (toggleDivContainer.classList.contains("hidden")) {
+      toggleDivContainer.classList.remove("hidden");
+      body.classList.add("no-scroll");
+    } else {
+      toggleDivContainer.classList.add("hidden");
+      body.classList.remove("no-scroll");
+    }
+  });
+});
+
+document.addEventListener("click", function (event) {
+  const toggleDivContainer = document.querySelector("#toggleDiv").parentElement;
+  const specificDiv = document.querySelector(".content-container");
+  const body = document.body;
+
+  const isClickInsideToggleDiv = toggleDivContainer.contains(event.target);
+  const isClickInsideSpecificDiv =
+    specificDiv && specificDiv.contains(event.target);
+  const isToggleButton = event.target.closest(".toggleButton");
+
+  if (!isToggleButton && !isClickInsideSpecificDiv) {
+    toggleDivContainer.classList.add("hidden");
+    body.classList.remove("no-scroll");
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  const toggleDivContainer = document.querySelector("#toggleDiv").parentElement;
+  const body = document.body;
+  if (event.key === "Escape") {
+    toggleDivContainer.classList.add("hidden");
+    body.classList.remove("no-scroll");
+  }
 });
